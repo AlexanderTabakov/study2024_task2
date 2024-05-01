85e74252d3b94f07f627e1af0127f0675de8af87
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2024-05-01 17:38:59 +0700

    очистка Мейн

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import {App} from "app/App";
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from "react-router-dom";
-import Input from "ui/Input";
-import FileInput from "ui/FileInput";
+
 
 const root = (
         <BrowserRouter>
-            <Input  type={'text'} isDisabled={false}  onUpdate={()=> {
-                console.log('111')}}   />
-
-            <FileInput/>
+            <App/>
         </BrowserRouter>
 )
 
